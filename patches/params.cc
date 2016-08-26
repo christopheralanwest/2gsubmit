ef6bc1fd03d031e812efa4d2422e5e8abac2b4fd
Chris West <christopher.alan.west@gmail.com>
2016-08-26 16:38:48 +0200

    Fix scale variations, increase NNLO iterations, remove extraneous semicolons

--- patches/params.cc
+++ patches/params.cc
@@ -13,12 +13,15 @@
 >         else if (token=="ph2_eta_max") str >> ph2_eta_max;
 >         else if (token=="ph1_eta_min") str >> ph1_eta_min;
 >         else if (token=="ph2_eta_min") str >> ph2_eta_min;
-142a155,158
+115a128,129
+> 	else if (token=="renscalefactor") str >> renscalefactor;
+> 	else if (token=="facscalefactor") str >> facscalefactor;
+142a157,160
 >     std::cout << "ph1_eta_max : " << ph1_eta_max << std::endl;
 >     std::cout << "ph2_eta_max : " << ph2_eta_max << std::endl;
 >     std::cout << "ph1_eta_min : " << ph1_eta_min << std::endl;
 >     std::cout << "ph2_eta_min : " << ph2_eta_min << std::endl;
-151a168,171
+151a170,173
 >     std::cout << "Renormalization and factorization scale factors (to compute uncertainties) " << std::endl;
 >     std::cout << "renscalefactor: "<< renscalefactor << std::endl;
 >     std::cout << "facscalefactor: "<< facscalefactor << std::endl;
--- submitJobs.sh
+++ submitJobs.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-basedir=`pwd`;
-location='CERN';
-#regionsArray='PRL';
-#regionsArray='BB';
-regionsArray='BE BB';
-#contribsArray='BORN';
-contribsArray='BORN NLO NNLO';
-renfacscalesArray='R2F2 R1F1 R0p5F0p5';
-#renfacscalesArray='R2F2 R0p5F0p5';
-#renfacscalesArray='R1F1';
-massbinsArray='Mass0p23To2TeV';
-#massbinsArray='Mass0p02To0p2TeV';
-#massbinsArray='Mass0p2To2TeV';
-#massbinsArray='Mass0p3To1TeV Mass1To2TeV';
-#massbinsArray='Mass0p15To1TeV Mass1To2TeV Mass2To3TeV Mass3To4TeV';
-##massbinsArray='Mass0p3To1TeV';
+basedir=`pwd`
+location='CERN'
+#regionsArray='PRL'
+regionsArray='BB'
+#regionsArray='BE BB'
+#contribsArray='BORN'
+contribsArray='BORN NLO NNLO'
+#renfacscalesArray='R2F2 R1F1 R0p5F0p5'
+#renfacscalesArray='R2F2 R0p5F0p5'
+renfacscalesArray='R1F1'
+massbinsArray='Mass0p23To2TeV'
+#massbinsArray='Mass0p02To0p2TeV'
+#massbinsArray='Mass0p2To2TeV'
+#massbinsArray='Mass0p3To1TeV Mass1To2TeV'
+#massbinsArray='Mass0p15To1TeV Mass1To2TeV Mass2To3TeV Mass3To4TeV'
+##massbinsArray='Mass0p3To1TeV'
 # name of 2gNNLO directory
 DIRNAME="2gNNLO_2.1"
 
@@ -31,27 +31,27 @@ if [[ "$location" == "CERN" ]]; then
 fi
 
 mkdir -p $basedir/JOBSUBMISSION
-DIRDATE=$(date +%F-%T | sed 's/:/-/g');
+DIRDATE=$(date +%F-%T | sed 's/:/-/g')
 mkdir -p $basedir/JOBSUBMISSION/$DIRDATE
 
 numjobs=50
 
-for k in $renfacscalesArray;
+for k in $renfacscalesArray
 do
-    for i in $regionsArray;
+    for i in $regionsArray
     do
-	for m in $massbinsArray;
+	for m in $massbinsArray
 	do
-	    for j in $contribsArray;
+	    for j in $contribsArray
 	    do
 		cd $basedir
 		mkdir -p SUBMITDIR
 		rm -rf SUBMITDIR/*
-		cd SUBMITDIR;
+		cd SUBMITDIR
 		cp -r $basedir/$DIRNAME .
 		cd $DIRNAME
-		echo $i"_"$j"_"$k"_"$m;
-		echo category $i"_"$j"_"$k"_"$m"_13TeV";
+		echo $i"_"$j"_"$k"_"$m
+		echo category $i"_"$j"_"$k"_"$m"_13TeV"
 		$basedir/createConfigs.sh $basedir/input.txt.TEMPLATE $i"_"$j"_"$k"_"$m"_13TeV" START
 		cp $basedir/SUBMITDIR/$DIRNAME/input.txt bin
                 # hack to change PDF
@@ -63,32 +63,33 @@ do
                     sed -i 's/MSTW2008lo68cl.LHgrid/CT10nlo.LHgrid/g' "src/pdf_lha.f"
                 fi
 		echo "Current directory: "`pwd`
-		make clean;
-		make install;
+		make clean
+		make install
 		if [[ "$j" == *"BORN"* ]]; then
-		    numjobs=10;
+		    numjobs=10
+		elif [[ "$j" == *"NNLO"* ]]; then
+		    numjobs=100
 		else
-		    numjobs=50;
+		    numjobs=50
 		fi
-		echo $numjobs "jobs to be submitted for this task";
+		echo $numjobs "jobs to be submitted for this task"
 		mkdir -p $basedir/JOBSUBMISSION/$DIRDATE/$i"_"$j"_"$k"_"$m"_13TeV"
 		for (( l=1; l<=$numjobs; l++ ))
 		do
+		    # create job directory and copy files into it
 		    mkdir -p $basedir/JOBSUBMISSION/$DIRDATE/$i"_"$j"_"$k"_"$m"_13TeV"/"Job"$l
-		    ##echo "my index is" $l;
-		    cp -r * $basedir/JOBSUBMISSION/$DIRDATE/$i"_"$j"_"$k"_"$m"_13TeV"/"Job"$l;
-		    cp -r $basedir/$DIRNAME $basedir/JOBSUBMISSION/$DIRDATE/$i"_"$j"_"$k"_"$m"_13TeV"/"Job"$l/bin;
-		    cp $basedir/run.sh $basedir/JOBSUBMISSION/$DIRDATE/$i"_"$j"_"$k"_"$m"_13TeV"/"Job"$l/bin;
-		    cd $basedir/JOBSUBMISSION/$DIRDATE/$i"_"$j"_"$k"_"$m"_13TeV"/"Job"$l/bin;
-		    ##pwd;ls;
+		    cp -r * $basedir/JOBSUBMISSION/$DIRDATE/$i"_"$j"_"$k"_"$m"_13TeV"/"Job"$l
+		    cp -r $basedir/$DIRNAME $basedir/JOBSUBMISSION/$DIRDATE/$i"_"$j"_"$k"_"$m"_13TeV"/"Job"$l/bin
+		    cp $basedir/run.sh $basedir/JOBSUBMISSION/$DIRDATE/$i"_"$j"_"$k"_"$m"_13TeV"/"Job"$l/bin
+		    cd $basedir/JOBSUBMISSION/$DIRDATE/$i"_"$j"_"$k"_"$m"_13TeV"/"Job"$l/bin
 		    if [[ "$j" == *"NNLO"* ]]; then
-			echo "Submitting to queue "$longQueue;
+			echo "Submitting to queue "$longQueue
 			bsub -q $longQueue -J "job"$l"_"$i"_"$j"_"$k"_13TeV" -o output.txt `pwd`/run.sh
 		    else
-			echo "Submitting to queue "$mainQueue;
+			echo "Submitting to queue "$mainQueue
 			bsub -q $mainQueue -J "job"$l"_"$i"_"$j"_"$k"_13TeV" -o output.txt `pwd`/run.sh
 		    fi
-		    cd -;
+		    cd -
 		done ##end of loop over number of jobs
 	    done ##end of loop over contributions (BORN, NLO, NNLO)
 	done ## end of loop over mass bins (0.3-1, 1-2, 2-3, 3-4 TeV)
